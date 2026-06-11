d7da29a23d542d2b23aca679187de17a0772acf1
lhea <lhea@blueharbor-media.com>
2026-06-11 08:39:07 +0800

    Preload bg.png so background shows on first load without refresh

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link rel="preload" as="image" href="/bg.png" />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
